e8268d5957da04afca4a9ee53478c826e5a0dffa
MZen2610 <mzen1980@gmail.com>
2021-10-31 19:49:08 +0300

    Задержка запуска программы выставлена в сутки

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import time
 from urllib.parse import urlsplit, unquote
 from os.path import split, splitext
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from os import listdir
+from os.path import isfile
+from os.path import join as joinpath
 
 import requests
 import os
@@ -41,46 +45,62 @@ def fetch_spacex_last_launch(url: str):
 
 if __name__ == "__main__":
     load_dotenv()
-    token = os.environ["NASA_TOKEN"]
+    nasa_token = os.environ["NASA_TOKEN"]
+    tgm_token = os.environ["TGM_TOKEN"]
 
-    url = "https://api.spacexdata.com/v3/launches?flight_number=94"
-    responsespacex = None
-    responsespacex = fetch_spacex_last_launch(url)
-    if not responsespacex is None:
-        images = responsespacex[0]["links"]["flickr_images"]
+    while True:
+        time.sleep(10)
+
+        url = "https://api.spacexdata.com/v3/launches/past"
+        data_spacex = None
+        data_spacex = fetch_spacex_last_launch(url)
+        step = 1
+        images = data_spacex[-step]["links"]["flickr_images"]
+        while len(images) == 0:
+            step += 1
+            images = data_spacex[-step]["links"]["flickr_images"]
+
         for image_number, image_url in enumerate(images):
             extension = get_extension(image_url)
             filename = f"spacex{image_number + 1}{extension}"
             download_image(image_url, filename)
 
-    url = f"https://api.nasa.gov/planetary/apod?api_key={token}&count=30"
-    data_spacenasa = None
-    data_spacenasa = fetch_spacex_last_launch(url)
-    if not data_spacenasa is None:
+        url = f"https://api.nasa.gov/planetary/apod?api_key={nasa_token}&count=30"
+        data_spacenasa = None
+        data_spacenasa = fetch_spacex_last_launch(url)
         for image_number, image_url in enumerate(data_spacenasa):
             extension = get_extension(image_url["url"])
             filename = f"spacenasa{image_number + 1}{extension}"
             download_image(image_url["url"], filename)
 
-    today = "2021-10-21"
-    url = f"https://api.nasa.gov/EPIC/api/natural/date/{today}?api_key={token}"
-    data_space_epic = None
-    data_space_epic = fetch_spacex_last_launch(url)
-    if not data_space_epic is None:
+        today = datetime.date(datetime.today())
+        url = f"https://api.nasa.gov/EPIC/api/natural/date/{today}?api_key={nasa_token}"
+        data_space_epic = None
+        data_space_epic = fetch_spacex_last_launch(url)
+        step = 1
+        while len(data_space_epic) == 0:
+            result_date = today - timedelta(days=step)
+            url = f"https://api.nasa.gov/EPIC/api/natural/date/{result_date}?api_key={nasa_token}"
+            data_space_epic = fetch_spacex_last_launch(url)
+            step += 1
+
         for image_number, image_url in enumerate(data_space_epic):
             image_date = datetime.fromisoformat(image_url["date"]).strftime(
                 "%Y/%m/%d")
             image = image_url["image"]
             url = f"https://api.nasa.gov/EPIC/archive/natural/{image_date}/png" \
-                  f"/{image}.png?api_key={token}"
+                  f"/{image}.png?api_key={nasa_token}"
             filename = f"space_epic{image_number + 1}.png"
             download_image(url, filename)
 
-    token = os.environ["TGM_TOKEN"]
-    bot = telegram.Bot(token=token)
-    updates = bot.get_updates()
-    if len(updates) > 0:
-        chat_id = updates[-1].effective_chat.id
-        bot.send_message(chat_id=chat_id, text="First message")
-        bot.send_document(chat_id=chat_id, document=open(
-            'images/space_epic1.png', 'rb'))
+        bot = telegram.Bot(token=tgm_token)
+        updates = bot.get_updates()
+        if len(updates) > 0:
+            chat_id = updates[-1].effective_chat.id
+            mypath = "images"
+            for path_to_file in listdir(mypath):
+                time.sleep(40)  # телеграмм ругается если добавлять изображения
+                # чаще 40 секунд
+                if isfile(joinpath(mypath, path_to_file)):
+                    bot.send_document(chat_id=chat_id, document=open(
+                        f"{mypath}/{path_to_file}", 'rb'))
