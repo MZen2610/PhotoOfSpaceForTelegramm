1c6154bbaa3be7da052248d580b767a28f7e8644
MZen2610 <mzen1980@gmail.com>
2021-11-14 08:54:08 +0300

    убрал закоментированный код

--- main.py
+++ main.py
@@ -93,7 +93,6 @@ def publish_telegramm_photos(token, chat_id, path):
     for path_to_file in listdir(path):
         if isfile(joinpath(path, path_to_file)):
             with open(f"{path}/{path_to_file}", 'rb') as file:
-                # bot.send_document(chat_id=chat_id, document=file.read())
                 bot.send_document(chat_id=chat_id, document=file)
                 time.sleep(86400)
 
@@ -106,9 +105,9 @@ def main():
 
     while True:
         try:
-            # download_spacex_photos()
-            # download_space_nasa_photos(nasa_token)
-            # download_space_epic_photos(nasa_token)
+            download_spacex_photos()
+            download_space_nasa_photos(nasa_token)
+            download_space_epic_photos(nasa_token)
             publish_telegramm_photos(tgm_token, chat_id, "images")
         except requests.exceptions.HTTPError:
             print("Проверьте вводимый адрес")
